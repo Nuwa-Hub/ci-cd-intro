ba8473541cf9f9c6b5e2c9b7ceef5af40c474f2c
nuwan <nuwandeshapriyaaa@gmail.com>
2024-01-03 14:42:16 +0530

    for testing

--- web/routes/index.js
+++ web/routes/index.js
@@ -9,6 +9,7 @@ router.get('/', function (req, res, next) {
 /* GET ice-flakes resource */
 router.get('/ice-flakes', function (req, res, next) {
   res
+  
     .status(205)
     .json({
       resource: 'ice-flakes',
